2fb89c41086765e85fc4238eed865b74abdd75d3
Alexandros Kritikos <akritikos@outlook.com>
2021-04-13 14:11:52 +0300

    🚨 Uses DbContextOptions properly & disables nullable context for class

--- src/Sphinx.Data.Persistence/DataProtectionDbContext.cs
+++ src/Sphinx.Data.Persistence/DataProtectionDbContext.cs
@@ -1,17 +1,16 @@
+#nullable disable
 namespace Kritikos.Sphinx.Data.Persistence
 {
-
   using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
   using Microsoft.EntityFrameworkCore;
 
   public class DataProtectionDbContext : DbContext, IDataProtectionKeyContext
   {
-    public DataProtectionDbContext(DbContextOptions options)
+    public DataProtectionDbContext(DbContextOptions<DataProtectionDbContext> options)
       : base(options)
     {
     }
 
     public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
-
   }
 }
