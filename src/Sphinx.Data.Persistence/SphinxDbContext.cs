fc9e1fdcbdbb0227209c6a06214868d454b4c17c
George Leonidis <esentakos@yahoo.gr>
2021-04-27 13:15:03 +0300

    🔧Configures Sphinx context with models

--- src/Sphinx.Data.Persistence/SphinxDbContext.cs
+++ src/Sphinx.Data.Persistence/SphinxDbContext.cs
@@ -4,6 +4,7 @@ namespace Kritikos.Sphinx.Data.Persistence
 
   using Kritikos.Configuration.Peristence.IdentityServer;
   using Kritikos.Sphinx.Data.Persistence.Identity;
+  using Kritikos.Sphinx.Data.Persistence.Models;
 
   using Microsoft.EntityFrameworkCore;
 
@@ -12,6 +13,46 @@ namespace Kritikos.Sphinx.Data.Persistence
     public SphinxDbContext(DbContextOptions options)
       : base(options)
     {
+    }
+
+    public DbSet<Stimulus> Stimuli { get; set; }
+
+    public DbSet<DataSet> DataSets { get; set; }
+
+    public DbSet<TestSession> TestSessions { get; set; }
+
+    public DbSet<SessionQuestion> SessionQuestions { get; set; }
+
+    public DbSet<InsignificantStimulus> InsignificantStimuli { get; set; }
+
+    public DbSet<SignificantStimulus> SignificantStimuli { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.Entity<Stimulus>(x =>
+      {
+        x.HasDiscriminator(y => y.isSignificant)
+            .HasValue<SignificantStimulus>(true)
+            .HasValue<InsignificantStimulus>(false)
+            .IsComplete();
+        x.Property(y => y.Type)
+            .HasConversion<string>();
+      });
+
+      modelBuilder.Entity<SessionQuestion>(x =>
+      {
+        x.HasOne(y => y.Session)
+            .WithMany()
+            .HasForeignKey("TestSessionId");
+        x.HasOne(y => y.Primary)
+            .WithMany()
+            .HasForeignKey("PrimaryId");
+        x.HasOne(y => y.Secondary)
+            .WithMany()
+            .HasForeignKey("SecondaryId");
+        x.HasKey("TestSessionId", "PrimaryId", "SecondaryId");
+      });
     }
   }
 }
