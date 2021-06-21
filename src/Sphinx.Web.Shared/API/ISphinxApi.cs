30c1db837dc040a5efacb2567bf1d06014c2a84d
just-sudo-it <lefteris.michailides@gmail.com>
2021-06-21 22:48:06 +0300

    adds method stubs for tests retrieval in API interface

--- src/Sphinx.Web.Shared/API/ISphinxApi.cs
+++ src/Sphinx.Web.Shared/API/ISphinxApi.cs
@@ -1,13 +1,22 @@
 namespace Kritikos.Sphinx.Web.Shared.API
 {
+  using System;
   using System.Collections.Generic;
+  using System.Threading;
   using System.Threading.Tasks;
 
+  using Kritikos.Sphinx.Web.Shared.RetrieveDto;
   using Refit;
 
   public interface ISphinxApi
   {
     [Get("/WeatherForecast")]
     Task<List<WeatherForecast>> GetForecast();
+
+    [Get("api/testsession")]
+    Task<PagedResult<TestSessionRetrieveDto>> GetTestSessions();
+
+    [Get("api/testsession/{id}")]
+    Task<TestSessionRetrieveDto> GetTestSession(Guid id, CancellationToken cancellationToken = default);
   }
 }
